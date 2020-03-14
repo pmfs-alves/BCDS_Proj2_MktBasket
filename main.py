506e8ea059848b7120fb786be6c1fbe08de55c93
ajose98 <a.sofia.c.j@gmail.com>
2020-03-14 15:23:25 +0000

    join das cidades, novo csv
    pus uma nova pasta e pus os ficheiros na pasta mais adequada, para não estar tudo ao molho.

--- main.py
+++ main.py
@@ -12,11 +12,28 @@ pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
 
 # ------------------------------------------- Import file and initial check --------------------------------------------
-df1 = pd.read_csv('./data/AsianRestaurant_Cyprus_2018.txt', sep=';')
-df2 = pd.read_csv('./data/AsianRestaurant_Cyprus_2018_partial.txt', sep=';')
-df1.dtypes
+df = pd.read_csv('./data/AsianRestaurant_Cyprus_2018.txt', sep=';')
+#df2 = pd.read_csv('./data/AsianRestaurant_Cyprus_2018_partial.txt', sep=';')
+df.dtypes
+
+# remove spaces at the end/beginning of strings
+df['CustomerCity'] = df.CustomerCity.str.strip()
+
+# there are several ways to address the same city so we will normalize the cities names
+# import file with each way and its normalized form + lat/long + distance to Nicosia
+norm_cities = pd.read_excel (r'./data/CityChanges.xlsx')
+# all cities in our dataframe are upper case
+norm_cities['City'] = norm_cities['City'].str.upper()
+# merge norm_cities with df
+df1 = df.merge(norm_cities, how='left',left_on='CustomerCity',right_on='City')
+# drop unnecessary columns and rename the normalized city column
+df1.drop(columns=['CustomerCity','City'], inplace=True)
+df1.rename(columns={'new_name':'CustomerCity'}, inplace=True)
+
 # correct TotalAmount data type
 df1.TotalAmount = df1.TotalAmount.apply(lambda x: float(str(x.replace(',', '.'))))
+
+
 df1.describe(include="all")
 # Checking missing values
 # só customer city e customer since têm NULLS
@@ -129,7 +146,7 @@ familydf = pd.concat([df1.groupby(["ProductFamily"])["Qty"].sum(), df1.groupby([
 familydf
 
 # In the same invoice we can have duplicates (e.g. ordering a Coke at the beginning and middle of a meal)
-df1[(df1.DocNumber == 110044742018) & (df1.ProductDesignation == 'COKE')]
+df1[(df1.DocNumber == 'TK0110000022018') & (df1.ProductDesignation == 'CARLSBERG 33CL')]
 
 # How many Pax in a delivery order?
 df1[df1.IsDelivery == 1].Pax.value_counts()  # always Pax=1 for IsDelivery=1 customers
@@ -160,6 +177,23 @@ df1.loc[(df1["IsDelivery"] == 1) & (df1["ProductDesignation"] == "DELIVERY CHARG
 df1.loc[(df1["IsDelivery"] == 1)].groupby("DocNumber").size()  # - 4866 invoices
 # Not every IsDelivery=1 invoice has Delivery Charge
 
+#----------------------
+# MERGE ROWS
+#----------------------
+
+# Sum quantity and total amount for lines of an invoice which have the same product
+qty_amount = df1[['DocNumber','ProductDesignation','Qty','TotalAmount']].groupby(['DocNumber','ProductDesignation']).sum()
+
+# get columns to use
+columns = list(df1.columns)
+columns.remove('TotalAmount')
+columns.remove('Qty')
+
+# drop duplicates from the df (keep first) and create df with all columns except TotalAmount and Qty
+family_deliv = df1.drop_duplicates(subset=['DocNumber','ProductDesignation'],keep= 'first')[columns]
+
+# merge the 2 df's created above
+df_clean = family_deliv.merge(qty_amount, how='left',on=['DocNumber','ProductDesignation'])
 
 #----------------------
 # FEATURE ENGINEERING
@@ -168,32 +202,32 @@ df1.loc[(df1["IsDelivery"] == 1)].groupby("DocNumber").size()  # - 4866 invoices
 # CREATE WEEKDAY ATTRIBUTE
 
 # Create attribute InvoiceDateHour_time -> turn InvoiceDateHour to type Datetime
-df1['InvoiceDateHour_time'] =  pd.to_datetime(df1['InvoiceDateHour'], format='%Y-%m-%d %H:%M:%S.%f')
+df_clean['InvoiceDateHour_time'] =  pd.to_datetime(df_clean['InvoiceDateHour'], format='%Y-%m-%d %H:%M:%S.%f')
 # Drop original column
-df1.drop(columns=['InvoiceDateHour'],inplace=True)
+df_clean.drop(columns=['InvoiceDateHour'],inplace=True)
 # Create list with weekdays
 weekDays = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
 # Create new column with weekday name
-df1['Weekday'] = df1.InvoiceDateHour_time.apply(lambda x: weekDays[x.weekday()])
+df_clean['Weekday'] = df_clean.InvoiceDateHour_time.apply(lambda x: weekDays[x.weekday()])
 
 # CREATE MEAL (LUNCH/DINNER) ATTRIBUTE
 
 # Create attribute which only shows the hour of the invoice
-df1['InvoiceHour'] = df1.InvoiceDateHour_time.apply(lambda x: x.hour)
+df_clean['InvoiceHour'] = df_clean.InvoiceDateHour_time.apply(lambda x: x.hour)
 # See distribution of hours, given if it is delivery or dine inn
-df1.groupby(by='IsDelivery')['InvoiceHour'].value_counts()
+df_clean.groupby(by='IsDelivery')['InvoiceHour'].value_counts()
 # Create attribute
-df1['Meal'] = df1.InvoiceHour.apply(lambda x: 'Lunch' if x<=17 and x>10 else 'Dinner')
+df_clean['Meal'] = df_clean.InvoiceHour.apply(lambda x: 'Lunch' if x<=17 and x>10 else 'Dinner')
 
 # CREATE HOLIDAY ATTRIBUTE
 
 # Create attribute without the hour, min, sec
-df1['Date'] = df1.InvoiceDateHour_time.apply(lambda x: x.date())
+df_clean['Date'] = df_clean.InvoiceDateHour_time.apply(lambda x: x.date())
 # Create list with dates of holidays for 2018 in cyprus
 holidays = ['01 Jan 2018','06 Jan 2018','19 Feb 2018','25 Mar 2018','01 Apr 2018','06 Apr 2018','07 Apr 2018','08 Apr 2018','09 Apr 2018','01 May 2018','28 May 2018','15 Aug 2018','01 Oct 2018','28 Oct 2018','24 Dec 2018','25 Dec 2018','26 Dec 2018','31 Dec 2018']
 holidays_date = pd.to_datetime(holidays,format='%d %b %Y')
 # Create final attribute
-df1['Holiday'] = df1.Date.apply(lambda x: 1 if x in holidays_date else 0)
+df_clean['Holiday'] = df_clean.Date.apply(lambda x: 1 if x in holidays_date else 0)
 
 # CREATE SEASON
 
@@ -210,35 +244,25 @@ def get_season(data):
         return 'Winter'
 
 # Create final attribute
-df1['Season'] = df1.Date.apply(get_season)
+df_clean['Season'] = df_clean.Date.apply(get_season)
+# drop unnecessary column Date
+df_clean.drop(columns='Date', inplace=True)
 
 # CREATE WEEKEND ATTRIBUTE
-df1['Weekend'] = df1.Weekday.apply(lambda x: 1 if x in ['Saturday','Sunday'] else 0)
+df_clean['Weekend'] = df_clean.Weekday.apply(lambda x: 1 if x in ['Saturday','Sunday'] else 0)
 
 # CREATE ATTRIBUTES RELATED TO WEATHER
 
 # do web scrapping
 # https://www.wunderground.com/history/monthly/cy/τύμβου/LCEN/date/2018-7
 
-#----------------------
-# MERGE ROWS
-#----------------------
-
-# Sum quantity and total amount for lines of an invoice which have the same product
-qty_amount = df1[['DocNumber','ProductDesignation','Qty','TotalAmount']].groupby(['DocNumber','ProductDesignation']).sum()
-
-# drop duplicates from the df (keep first) and create df with columns 'DocNumber','ProductDesignation','ProductFamily','IsDelivery'
-family_deliv = df1.drop_duplicates(subset=['DocNumber','ProductDesignation'],keep= 'first')[['DocNumber','ProductDesignation','ProductFamily','IsDelivery',
-                                    'Season','Meal','Holiday','Weekend','Weekday']]
-
-# merge the 2 df's created above
-df_clean = family_deliv.merge(qty_amount, how='left',on=['DocNumber','ProductDesignation'])
 
 # Delete useless variables
-del df2,family_deliv, holidays, holidays_date, qty_amount, weekDays
+del family_deliv, holidays, holidays_date, qty_amount, weekDays,columns, df1, df, norm_cities
+
 
 # Save df to csv file
-#df_clean.to_csv(r'C:\Users\Sofia\OneDrive - NOVAIMS\Nova IMS\Mestrado\2º semestre\Business Cases\BC3\BCDS_Proj2_MktBasket\data_cyprus.csv', index = False)
+df_clean.to_csv(r'./data/data_cyprus.csv', index = False)
 
 #----------------------
 # ONE HOT ENCODING
